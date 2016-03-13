f66b7e3740fd9d7a6f53b03aa31b4f16ec891a5c
Julian Scharf <julzelements@gmail.com>
2016-03-13 17:01:47 +1000

    written most of func: alienGoodsValueInput

--- MerchantsGuideToTheGalaxy/RegexPlayground.playground/Contents.swift
+++ MerchantsGuideToTheGalaxy/RegexPlayground.playground/Contents.swift
@@ -11,27 +11,22 @@ let romanNumerals = merchantsDictionarys.romanNumerals
 var alienNumerals = merchantsDictionarys.alienNumerals
 var alienGoods = merchantsDictionarys.alienGoods
 
-func processUserInput(statements: [String], merchantDictionary: MerchantsGuideDictionarys) {
+func handleUserStatements(statements: [String], merchantDictionary: MerchantsGuideDictionarys, handler: MerchantsGuideInputHandler) {
+  let RNs = merchantDictionary.romanNumerals
   for statement in statements {
-    let statementType = merchantsInputHandler.evaluateUserInputString(statement, romanNumerals: merchantDictionary.romanNumerals)
+    let statementType = handler.evaluateUserInputString(statement, romanNumerals: RNs)
     switch statementType {
-    case .AlienNumeralValueInput: 
+    case .AlienNumeralValueInput: print("\(statement) is Alien Numeral Value Input")
+      let newAlienNumeral = handler.retrieveAlienNumeralValue(statement, romanNumerals: RNs)
+      alienNumerals[newAlienNumeral.alienNumeral] = newAlienNumeral.alienValue
+    case .AlienGoodsValueInput: print("\(statement) is Alien Goods Value Input")
+    case .AlienNumeralQuery: print("\(statement) is Alien Numeral Query")
+    case .AlienGoodsQuery: print("\(statement) is Alien Goods Query")
+    case .InvalidUserStatement: print("\(statement) is invalid user input")
     }
-    }
+    
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -42,6 +37,19 @@ let testInputB = "glob glob silver is 34 Credits"
 let testInputC = "how much is pish tegj glob glob ?"
 let testInputD = "how many Credits is glob prok Silver ?"
 let testInputE = "how much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+
+let testAlienDictionary = ["glob": 1, "prok": 5, "pish": 10, "tegj": 50]
+
+merchantsInputHandler.getAlienNumeralsFromSentence(testInputC, alienNumeralDictionary: testAlienDictionary)
+merchantsInputHandler.getIntegerFromSentence(testInputB)
+
+handleUserStatements([testInputA, testInputB, testInputC, testInputD, testInputE], merchantDictionary: merchantsDictionarys, handler: merchantsInputHandler)
+
+print(alienNumerals)
+
+
+
+
 
 
 
--- MerchantsGuideToTheGalaxy/RegexPlayground.playground/Sources/MerchantsGuideInputHandler.swift
+++ MerchantsGuideToTheGalaxy/RegexPlayground.playground/Sources/MerchantsGuideInputHandler.swift
@@ -6,15 +6,15 @@ public class MerchantsGuideInputHandler {
     
   }
   
-  public enum TypeOfUserStatement {
+  public enum SentenceType {
     case AlienNumeralValueInput
     case AlienGoodsValueInput
     case AlienNumeralQuery
     case AlienGoodsQuery
-    case InvalidUserStatement
+    case InvalidUserSentence
   }
   
-  public func evaluateUserInputString(inputString: String, romanNumerals: [String: Int]) -> TypeOfUserStatement {
+  public func evaluateUserInputString(inputString: String, romanNumerals: [String: Int]) -> SentenceType {
     if isAlienNumeralValue(inputString, romanNumerals: romanNumerals) {
       return .AlienNumeralValueInput
     } else if isAlienGoodsValue(inputString) {
@@ -24,9 +24,8 @@ public class MerchantsGuideInputHandler {
     } else if isAlienGoodsQuery(inputString) {
       return .AlienGoodsQuery
     }
-    return .InvalidUserStatement
+    return .InvalidUserSentence
   }
-
 
   func isAlienNumeralValue(inputString: String, romanNumerals: [String: Int]) -> Bool {
     var isValidQuery = false
@@ -63,7 +62,7 @@ public class MerchantsGuideInputHandler {
     return isValidQuery
   }
   
-  public func handleAlienNumeralValue(inputString: String, romanNumerals: [String: Int]) -> (alienNumeral: String, alienValue: Int) {
+  public func retrieveAlienNumeralValue(inputString: String, romanNumerals: [String: Int]) -> (alienNumeral: String, alienValue: Int) {
     let words = inputString.componentsSeparatedByString(" ")
     var alienNumeral =  String()
     var alienValue = Int()
@@ -79,8 +78,13 @@ public class MerchantsGuideInputHandler {
     return (alienNumeral, alienValue)
   }
   
-  func handleAlienGoodsValue() {
-    
+  //This function parses a sentence of type .AlienGoodsValueInput
+  //This sentence type has the form: "glob glob Silver is 34 Credits"
+  //
+  func retrieveAlienGoodsValue(sentence: String, alienNumeralDictionary: [String: Int])-> (alienGood: String, alienGoodValue: Int) {
+    let alienNumeralPrice = getAlienNumeralsFromSentence(sentence, alienNumeralDictionary: alienNumeralDictionary)
+    //TODO:
+
   }
   
   func handleAlienNumeralQuery() {
@@ -90,5 +94,79 @@ public class MerchantsGuideInputHandler {
   func handleAlienGoodsQuery() {
     
   }
+  //This function scans an input sentence for alien Numerals that exist as keys in the alienNumeralDictionary
+  //It returns any found numerals ordered in an array
+  public func getAlienNumeralsFromSentence(sentence: String, alienNumeralDictionary: [String: Int])-> [String] {
+    let words = sentence.componentsSeparatedByString(" ")
+    let alienNumeralKeys = [String](alienNumeralDictionary.keys)
+    var alienNumerals = [String]()
+    for word in words {
+      if alienNumeralKeys.contains(word) {
+        alienNumerals.append(word)
+      }
+    }
+    return alienNumerals
+  }
+  
+  //This function scans an input sentence for any word that can be cast to an Int.
+  //Assumption: Only one or less possible Ints exist in the sentence
+  public func getIntegerFromSentence(sentence: String) -> Int? {
+    let words = sentence.componentsSeparatedByString(" ")
+    for word in words {
+      if let number = Int(word) {
+        return number
+      }
+    }
+    return nil
+  }
+  
+  //This function reads an array of Alien Numerals a dictionary and returns the value
+  //Example IO: input: (["prok", "glob", "glob"], ["glob": 1, "prok": 5])
+  //            output: 7
+  public func getAlienCompoundNumeralValue(alienNumerals: [String], alienNumeralDictionary: [String: Int]) -> Int {
+    var alienValues = [Int]()
+    for numeral in alienNumerals {
+      if let alienValue = alienNumeralDictionary[numeral] {
+        alienValues.append(alienValue)
+      }
+    }
+    return calculateRomanNumeral(romanNumeralInts: alienValues)
+  }
+  
+  //This function reads an array of integers as a roman numeral
+  //Example IO: input[10, 1, 5], output: 14
+  func calculateRomanNumeral(romanNumeralInts numerals: [Int]) -> Int {
+    var total = Int()
+    let reversed = Array(numerals.reverse())
+    
+    total += reversed[0]
+    for i in 1..<reversed.count {
+      let j = i - 1
+      if reversed[j] <= reversed[i] {
+        total += reversed[i]
+      } else if reversed[j] > reversed[i] {
+        total -= reversed[i]
+      }
+    }
+    return total
+  }
+  
+  
+  
   
 }
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
